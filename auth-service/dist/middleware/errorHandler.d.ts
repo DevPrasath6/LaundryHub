8838beb0a7dfd2ec2a6b803b4af24e1ece038a0e
Abinayaa018 <abinayaa.a2024cse@sece.ac.in>
2025-10-06 23:48:11 +0530

    Delete auth-service directory

--- auth-service/dist/middleware/errorHandler.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-export interface ApiError extends Error {
-    statusCode?: number;
-    isOperational?: boolean;
-}
-export declare const errorHandler: (err: ApiError, req: Request, res: Response, next: NextFunction) => void;
-export declare const createError: (message: string, statusCode: number) => ApiError;
-//# sourceMappingURL=errorHandler.d.ts.map
